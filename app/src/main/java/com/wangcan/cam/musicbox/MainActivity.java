b51994816cbf3f47738fe96ea39554a129404cc3
CAM <762092016@qq.com>
2018-11-16 08:38:05 +0800

    first commit

---
+++ app/src/main/java/com/wangcan/cam/musicbox/MainActivity.java
@@ -0,0 +1,80 @@
+package com.wangcan.cam.musicbox;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView textView;
+    Button playButton;
+    Button pauseButton;
+    Button stopButton;
+    ActivityRes receiver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textiew) ;
+        playButton = findViewById(R.id.playBtn);
+        stopButton = findViewById(R.id.stopBtn);
+        pauseButton = findViewById(R.id.pauseBtn);
+        playButton.setOnClickListener(this);
+        stopButton.setOnClickListener(this);
+        pauseButton.setOnClickListener(this);
+
+        //注册广播接受器
+        receiver = new ActivityRes();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(MUSIC_CHANGE_ACTION);
+        registerReceiver(receiver,filter);
+
+        Intent intent = new Intent(MainActivity.this,MusicService.class);
+        startService(intent);
+    }
+
+    public static final String MUSIC_CHANGE_ACTION = "MUSIC_CHANGE_ACTION_CAM";
+    public static final String MUSIC_NAME = "music_name_cam";
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        intent.setAction(MusicService.ACTION_SERVICE_RECEIVER);
+        switch (view.getId())
+        {
+            case R.id.pauseBtn:
+                intent.putExtra(MusicService.CODE,MusicService.PAUSE_STATUS);
+                break;
+            case R.id.stopBtn:
+                intent.putExtra(MusicService.CODE,MusicService.STOP_STATUS);
+                break;
+            case R.id.playBtn:
+                intent.putExtra(MusicService.CODE,MusicService.PLAY_STATUS);
+                break;
+        }
+        sendBroadcast(intent);
+    }
+
+    public class ActivityRes extends BroadcastReceiver
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            Toast.makeText(context, "new music", Toast.LENGTH_SHORT).show();
+            String s = intent.getStringExtra(MUSIC_NAME);
+            textView.setText(s);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(receiver);
+    }
+}
---
+++ app/src/main/java/com/wangcan/cam/musicbox/MusicService.java
@@ -0,0 +1,110 @@
+package com.wangcan.cam.musicbox;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class MusicService extends Service {
+
+    MediaPlayer player;
+    AssetManager manager;
+    String [] musics = {"beautiful.mp3","promise.mp3","wish.mp3"};
+    public static final int PLAY_STATUS = 0;
+    public static final int PAUSE_STATUS = 1;
+    public static final int STOP_STATUS = 2;
+    private int currentMusicIndex = 0;
+    private MusicReceiver receiver;
+
+    private static final String TAG = "CAM";
+    public MusicService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e(TAG,"onCreate");
+        manager = getAssets();
+        player = new MediaPlayer();
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                currentMusicIndex++;
+                currentMusicIndex%=musics.length;
+                prepareAndPlayMusic(musics[currentMusicIndex]);
+                Intent intent = new Intent();
+                intent.setAction(MainActivity.MUSIC_CHANGE_ACTION);
+                intent.putExtra(MainActivity.MUSIC_NAME,musics[currentMusicIndex]);
+                sendBroadcast(intent);
+            }
+        });
+
+        prepareAndPlayMusic(musics[currentMusicIndex]);
+        receiver = new MusicReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_SERVICE_RECEIVER);
+        registerReceiver(receiver,filter);
+
+    }
+
+    private void prepareAndPlayMusic(String music)
+    {
+        try {
+            AssetFileDescriptor asf = manager.openFd(music);
+            player.reset();
+            player.setDataSource(asf.getFileDescriptor(),asf.getStartOffset(),asf.getLength());
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static final String ACTION_SERVICE_RECEIVER = "ACTION_MUSIC_RECEIVER_RECEIVER_CAM";
+    public static final String CODE = "RECEIVER_CODE";
+    public class MusicReceiver extends BroadcastReceiver
+    {
+        @Override
+        public void onReceive(Context context, Intent intent)
+        {
+            int code = intent.getIntExtra(CODE,PLAY_STATUS);
+            switch (code)
+            {
+                case PLAY_STATUS:
+                    player.start();
+                    break;
+                case STOP_STATUS:
+                    player.stop();
+                    break;
+                case PAUSE_STATUS:
+                    player.pause();
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+}
